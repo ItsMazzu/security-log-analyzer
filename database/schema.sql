6a1471e6b07385d9f9d7ca3c9e983d85a652982a
Italo Mazzucatto <italo.itacom@gmail.com>
2026-03-09 21:16:10 -0300

    Add database schema

---
+++ database/schema.sql
@@ -0,0 +1,7 @@
+create table auth_logs (
+    id integer primary key autoincrement,
+    timestamp text not null,
+    username text not null,
+    ip_address text not null,
+    status text not null
+)
